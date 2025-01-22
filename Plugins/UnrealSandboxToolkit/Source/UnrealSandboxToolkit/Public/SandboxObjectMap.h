1a4d4497ae716baf6263d75f00aa33d0a6b74564
AlyonaMoskvina <alenamos7896@gmail.com>
2025-01-22 14:45:21 +0900

    twentieth code comment

--- Plugins/UnrealSandboxToolkit/Source/UnrealSandboxToolkit/Public/SandboxObjectMap.h
+++ Plugins/UnrealSandboxToolkit/Source/UnrealSandboxToolkit/Public/SandboxObjectMap.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Заполните информацию об авторских правах на странице описания настроек проекта.
 
 #pragma once
 
@@ -8,27 +8,32 @@
 #include "Runtime/Engine/Classes/Engine/DataAsset.h"
 #include "SandboxObjectMap.generated.h"
 
+// Определение структуры данных для статических данных песочницы
 USTRUCT(BlueprintType)
 struct FSandboxStaticData : public FTableRowBase {
 	GENERATED_BODY()
 
+	// Свойство для имени объекта, доступное для редактирования и чтения в Blueprint
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	FString ObjectName;
 };
 
-
+// Определение класса для карты объектов песочницы, наследуемого от UDataAsset
 UCLASS(BlueprintType, Blueprintable)
 class UNREALSANDBOXTOOLKIT_API USandboxObjectMap : public UDataAsset {
 	GENERATED_BODY()
-	
-public:	
 
+public:
+
+	// Свойство для списка объектов песочницы, доступное для редактирования в редакторе
 	UPROPERTY(EditAnywhere, Category = "UnrealSandbox Toolkit")
 	TArray<TSubclassOf<ASandboxObject>> ObjectList;
 
-	//UPROPERTY(EditAnywhere, Category = "UnrealSandbox Toolkit")
-	//TMap<uint64, FSandboxStaticData> StaticData;
+	// Закомментированное свойство для статических данных песочницы
+	// UPROPERTY(EditAnywhere, Category = "UnrealSandbox Toolkit")
+	// TMap<uint64, FSandboxStaticData> StaticData;
 
+	// Свойство для эффектов песочницы, доступное для редактирования в редакторе
 	UPROPERTY(EditAnywhere, Category = "UnrealSandbox Toolkit")
 	TMap<int32, TSubclassOf<ASandboxEffect>> Effects;
 
