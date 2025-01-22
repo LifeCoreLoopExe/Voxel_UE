ea7211c5ab10b7ff0b149cb71f31357e0c1ceeab
AlyonaMoskvina <alenamos7896@gmail.com>
2025-01-22 14:18:00 +0900

    ninth code comment

--- Plugins/UnrealSandboxToolkit/Source/UnrealSandboxToolkit/Private/SunPos.h
+++ Plugins/UnrealSandboxToolkit/Source/UnrealSandboxToolkit/Private/SunPos.h
@@ -1,38 +1,41 @@
-// This file is available in electronic form at http://www.psa.es/sdg/sunpos.htm
+// Этот файл доступен в электронном виде по адресу http://www.psa.es/sdg/sunpos.htm
 
-#ifndef __SUNPOS_H
-#define __SUNPOS_H
+#ifndef __SUNPOS_H  // Если __SUNPOS_H не определен
+#define __SUNPOS_H  // Определяем __SUNPOS_H, чтобы избежать повторного включения этого заголовочного файла
 
+// Объявление некоторых констант
+#define pi    3.14159265358979323846  // Число Пи
+#define twopi (2*pi)  // Дважды Пи
+#define rad   (pi/180)  // Константа для преобразования градусов в радианы
+#define dEarthMeanRadius     6371.01  // Средний радиус Земли в километрах
+#define dAstronomicalUnit    149597890  // Астрономическая единица в километрах
 
-// Declaration of some constants 
-#define pi    3.14159265358979323846
-#define twopi (2*pi)
-#define rad   (pi/180)
-#define dEarthMeanRadius     6371.01	// In km
-#define dAstronomicalUnit    149597890	// In km
-
+// Структура для хранения времени
 struct cTime
 {
-	int iYear;
-	int iMonth;
-	int iDay;
-	double dHours;
-	double dMinutes;
-	double dSeconds;
+    int iYear;  // Год
+    int iMonth;  // Месяц
+    int iDay;  // День
+    double dHours;  // Часы
+    double dMinutes;  // Минуты
+    double dSeconds;  // Секунды
 };
 
+// Структура для хранения географического положения
 struct cLocation
 {
-	double dLongitude;
-	double dLatitude;
+    double dLongitude;  // Долгота
+    double dLatitude;  // Широта
 };
 
+// Структура для хранения координат Солнца
 struct cSunCoordinates
 {
-	double dZenithAngle;
-	double dAzimuth;
+    double dZenithAngle;  // Зенитный угол
+    double dAzimuth;  // Азимут
 };
 
+// Функция для вычисления позиции Солнца
 void sunpos(cTime udtTime, cLocation udtLocation, cSunCoordinates *udtSunCoordinates);
 
-#endif
+#endif  // Конец условной компиляции
