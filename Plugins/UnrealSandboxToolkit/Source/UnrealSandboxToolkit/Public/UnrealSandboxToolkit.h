66f99425718b479b05c7431a59322b3f261da918
AlyonaMoskvina <alenamos7896@gmail.com>
2025-01-22 14:50:53 +0900

    twenty-second code comment

--- Plugins/UnrealSandboxToolkit/Source/UnrealSandboxToolkit/Public/UnrealSandboxToolkit.h
+++ Plugins/UnrealSandboxToolkit/Source/UnrealSandboxToolkit/Public/UnrealSandboxToolkit.h
@@ -1,15 +1,25 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Авторские права принадлежат Epic Games, Inc. Все права защищены.
 
 #pragma once
+// Директива препроцессора, предотвращающая множественное включение этого файла заголовка.
 
 #include "CoreMinimal.h"
+// Включение минимального набора основных заголовочных файлов Unreal Engine.
+
 #include "Modules/ModuleManager.h"
+// Включение заголовочного файла для управления модулями Unreal Engine.
 
 class FUnrealSandboxToolkitModule : public IModuleInterface
+// Объявление класса FUnrealSandboxToolkitModule, который наследуется от интерфейса IModuleInterface.
 {
 public:
 
 	/** IModuleInterface implementation */
+	// Реализация интерфейса IModuleInterface.
+
 	virtual void StartupModule() override;
+	// Виртуальная функция, вызываемая при запуске модуля. Переопределяет базовую реализацию.
+
 	virtual void ShutdownModule() override;
+	// Виртуальная функция, вызываемая при завершении работы модуля. Переопределяет базовую реализацию.
 };
