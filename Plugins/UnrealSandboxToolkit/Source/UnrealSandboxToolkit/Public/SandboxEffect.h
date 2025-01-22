7ddc0e9934e88675b785cdbeef5de34f9e345b30
AlyonaMoskvina <alenamos7896@gmail.com>
2025-01-22 14:35:03 +0900

    sixteenth code comment

--- Plugins/UnrealSandboxToolkit/Source/UnrealSandboxToolkit/Public/SandboxEffect.h
+++ Plugins/UnrealSandboxToolkit/Source/UnrealSandboxToolkit/Public/SandboxEffect.h
@@ -1,22 +1,19 @@
+#pragma once // Директива препроцессора, предотвращающая множественное включение этого заголовочного файла
 
-#pragma once
+#include "Engine.h" // Подключение заголовочного файла движка Unreal Engine
+#include "GameFramework/Actor.h" // Подключение заголовочного файла для базового класса AActor
+#include "SandboxEffect.generated.h" // Подключение сгенерированного заголовочного файла для этого класса
 
-#include "Engine.h"
-#include "GameFramework/Actor.h"
-#include "SandboxEffect.generated.h"
-
-UCLASS(BlueprintType, Blueprintable)
-class UNREALSANDBOXTOOLKIT_API ASandboxEffect : public AActor {
-	GENERATED_BODY()
-	
-public:	
-	ASandboxEffect();
-
-protected:
-
-	virtual void BeginPlay() override;
+UCLASS(BlueprintType, Blueprintable) // Макрос для объявления класса, который может быть использован в Blueprint
+class UNREALSANDBOXTOOLKIT_API ASandboxEffect : public AActor { // Объявление класса ASandboxEffect, наследующегося от AActor
+	GENERATED_BODY() // Макрос для генерации тела класса
 
 public:
+	ASandboxEffect(); // Конструктор класса
 
-	
+protected:
+	virtual void BeginPlay() override; // Переопределение метода BeginPlay, который вызывается при начале игры
+
+public:
+	// Здесь могут быть добавлены дополнительные публичные методы и переменные
 };
